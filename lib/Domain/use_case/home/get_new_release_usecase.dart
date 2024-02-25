1e24378d464844b5e7a95e6a305eaaf9b46a9bf9
fg <minamalak409@gmail.com>
2024-02-25 19:11:39 +0200

    finish

---
+++ lib/Domain/use_case/home/get_new_release_usecase.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:movie/Core/error/faliure.dart';
+import 'package:movie/Domain/repository/home/home_repository.dart';
+
+import '../../entity/home/get_movie_entity.dart';
+
+class GetNewReleaseUseCase{
+  HomeRepository homeRepository;
+  GetNewReleaseUseCase(this.homeRepository);
+  Future<Either<Failure,GetMovieEntity>>execute() => homeRepository.getNewRelease() ;
+}
